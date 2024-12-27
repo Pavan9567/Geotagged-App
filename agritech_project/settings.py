526c34030f13f2f196daf9ba7cba819d371e4980
Pavan9567 <pavank9567@gmail.com>
2024-12-27 09:29:42 +0530

    Geotagged-App

--- agritech_project/settings.py
+++ agritech_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +91,9 @@ WSGI_APPLICATION = 'agritech_project.wsgi.application'
 }"""
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
